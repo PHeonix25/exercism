c1cec0678f6017e0f438a67fa12f812f9d5e5c26
Pat Hermens <p@hermens.com.au>
2019-01-24 22:35:01 +0100

    Moved instantiation of mapping to class level for perf

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,12 +1,16 @@
-def dna_to_rna(dna):
-    return {
+# Hold the mapping in a static instance,
+# rather than per-instantiation.
+__dna_mapping__ = {
         'G':'C',
         'C':'G',
         'T':'A',
         'A':'U'
-    }.get(dna, '')
+    }
+
+def dna_to_rna(dna):
+    return __dna_mapping__.get(dna, '')
 
 def to_rna(dna_strand=''):
-    """Converts a DNA strand, to a matching RNA strand"""
+    """Converts a DNA strand to a matching RNA strand"""
 
     return ''.join(dna_to_rna(char) for char in dna_strand)
