86514f8945df5ad9c55ea66aab90e24a8ecc3a66
Pat Hermens <p@hermens.com.au>
2019-01-24 21:53:08 +0100

    Adding solution for DNA mapping

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,12 @@
-def to_rna(dna_strand):
-    pass
+def dna_to_rna(dna):
+    return {
+        'G':'C',
+        'C':'G',
+        'T':'A',
+        'A':'U'
+    }.get(dna, '')
+
+def to_rna(dna_strand=''):
+    """Converts a DNA strand, to a matching RNA strand"""
+
+    return ''.join(dna_to_rna(char) for char in dna_strand)
