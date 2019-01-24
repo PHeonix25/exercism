34712fc24c48574a3d5c188c9113cf5ffa8f4161
PHeonix25 <p@hermens.com.au>
2019-01-24 20:27:14 +0100

    Adding (YEARS OLD) F# solutions

---
+++ fsharp/hello-world/Library.fs
@@ -0,0 +1,5 @@
+namespace hello_world
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ fsharp/hello-world3/HelloWorldTest.fs
@@ -0,0 +1,9 @@
+﻿module HelloWorldTest
+
+open Xunit
+
+open HelloWorld
+
+[<Fact>]
+let ``Say hi!`` () =
+    Assert.Equal(hello, "Hello, World!")
