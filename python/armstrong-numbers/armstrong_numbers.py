c52123bdaff557e70dc06c2673971f9012268a47
Pat Hermens <p@hermens.com.au>
2019-01-24 21:12:47 +0100

    Adding solution for Armstrong Numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,8 @@
 def is_armstrong(number):
-    pass
+    total = 0
+    number_string = "{}".format(number)
+    
+    for num in number_string:
+        total += int(num) ** len(number_string)
+
+    return total == number
