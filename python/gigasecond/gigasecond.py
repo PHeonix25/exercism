fea2c64aa51f7b01f9278a590d7b21b935b59463
Pat Hermens <p@hermens.com.au>
2019-01-24 22:19:34 +0100

    Adding solution

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,9 @@
-def add_gigasecond(moment):
-    pass
+import datetime
+
+def add_gigasecond(moment=None):
+    """Calculate the moment when someone has lived for 10^9 seconds."""
+
+    if moment is None:
+        moment = datetime.datetime.now()
+    print(moment)
+    return moment + datetime.timedelta(seconds=10**9)
