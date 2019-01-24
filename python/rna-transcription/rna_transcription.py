d4a034dc91278e693bedd394069074c9fd744c40
Pat Hermens <p@hermens.com.au>
2019-01-24 23:09:47 +0100

    Made the code a LOT more performant

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,12 +1,3 @@
-# Hold the mapping in a static instance,
-# rather than per-instantiation.
-__dna_mapping__ = {
-        'G':'C',
-        'C':'G',
-        'T':'A',
-        'A':'U'
-    }
-
 def to_rna(dna_strand=''):
     """Converts a DNA strand to its matching RNA compliment."""
-    return ''.join(__dna_mapping__.get(char, '') for char in dna_strand)
+    return dna_strand.translate(str.maketrans("GCTA", "CGAU"))
