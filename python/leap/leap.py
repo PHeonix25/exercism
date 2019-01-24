9c4b1a899a29c5b819336ee848391a929ace069c
Pat Hermens <p@hermens.com.au>
2019-01-24 21:33:47 +0100

    Well, that was easier than expected

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,8 @@
-def is_leap_year(year):
-    pass
+def is_leap_year(year=1900):
+    """Determine whether a year is a leap year."""
+
+    # A "leap year occurs:
+    # on every year that is evenly divisible by 4
+    #   except every year that is evenly divisible by 100
+    #       unless the year is also evenly divisible by 400
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
