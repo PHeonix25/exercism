eef219624bcec525b108b42d8a48902dc68f79b4
Pat Hermens <p@hermens.com.au>
2019-01-27 21:08:22 +0100

    After a lot of trial and error with Regex!

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,5 @@
-def abbreviate(words):
-    pass
+import re
+
+def abbreviate(words=''):
+    """Abbreviates a given sentence to its acronym (_ignoring quotation marks_)"""
+    return ''.join(word[0].upper() for word in re.split(r"[^a-zA-Z']+", words) if word)
