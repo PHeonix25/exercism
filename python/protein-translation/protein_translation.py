3f0b43cc41d46e29901592e4c524c949737db2f6
Pat Hermens <p@hermens.com.au>
2019-01-27 22:32:28 +0100

    Updated solution to add explanation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -17,17 +17,17 @@ __codons__ = dict({
     "UGG":"Tryptophan"
 })
 
-def get_groups(strand):
-    """Returns chunks of 3 from the strand for easier comparison"""
-    args = [iter(strand)] * 3
-    return itertools.zip_longest(*args)
-
 def proteins(strand):
-    """Returns a list of the proteins that are in the strand using a functional approach"""
-    return list(__codons__.get(s) for s in itertools.takewhile( \
-        lambda x: __codons__.get(x) is not None, \
-        (''.join(chunk) for chunk in get_groups(strand))) \
-    )
+    """Returns a list of the proteins that are in the strand, using a functional approach"""
+    return list(__codons__.get(protein) for protein in \
+        itertools.takewhile( \
+            lambda x: __codons__.get(x) is not None, \
+            (''.join(chunk) for chunk in \
+                itertools.zip_longest(*([iter(strand)] * 3)))))
+                # This last line probaby deserves some explanation:
+                #   We're taking a reference to the same iterator, and
+                #   providing it three times to the same function so it'll:
+                #   step, step, step, return, step, step, step, return, etc.
 
 def proteins_procedural(strand):
     """Returns a list of the proteins that are in the strand using a very procedural approach"""
@@ -39,3 +39,7 @@ def proteins_procedural(strand):
         result.append(val)
 
     return result
+
+def get_groups(strand):
+    """Returns chunks of 3 from the strand for easier comparison"""
+    return itertools.zip_longest(*([iter(strand)] * 3))
