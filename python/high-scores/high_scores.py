65897c81feee04355253cc5dad68a65b20ca0093
Pat Hermens <p@hermens.com.au>
2019-01-31 15:02:53 +0100

    Final solution, with pythonic tuple selector

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,19 @@
 class HighScores(object):
     def __init__(self, scores):
-        pass
+        self.scores = scores
+    
+    def latest(self):
+        return self.scores[-1]
+
+    def personal_top(self):
+        return sorted(self.scores, reverse=True)[:3]
+
+    def personal_best(self):
+        return sorted(self.scores, reverse=True)[0]
+
+    def report(self):
+        last = self.latest()
+        best = self.personal_best()
+        diff = best - last
+        return "Your latest score was {}. ".format(last) + \
+            ("That's your personal best!", "That's {} short of your personal best!".format(diff))[diff > 0]
