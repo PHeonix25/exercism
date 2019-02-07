793c6c4fc9ae0af4e1afe3dc3958b4c92f832922
Pat Hermens <p@hermens.com.au>
2019-02-07 21:05:35 +0100

    Adding basic solution to challenge

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,18 @@
-class Matrix(object):
+class Matrix():
     def __init__(self, matrix_string):
-        pass
+        # I dislike building the array myself, but numpy isn't available...?
+        # Also, this could be "more pythonic", but it gets very confusing.
+        self.matrix = []
+        for rownum, row in enumerate(matrix_string.split('\n')):
+            self.matrix.append([])
+            for item in row.split(' '):
+                self.matrix[rownum].append(int(item))
 
-    def row(self, index):
-        pass
+    def row(self, index=1):
+        return self.matrix[index - 1]
 
-    def column(self, index):
-        pass
+    def column(self, index=1):
+        result = []
+        for rows in self.matrix:
+            result.append(rows[index - 1])
+        return result
