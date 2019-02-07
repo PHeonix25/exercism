a58297606216fde553d21bc3460b16d9b7c8993e
Pat Hermens <p@hermens.com.au>
2019-02-07 19:51:42 +0100

    Removed 'object', used MAX(), and f-strings

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,7 +1,7 @@
-class HighScores(object):
+class HighScores():
     def __init__(self, scores):
         self.scores = scores
-    
+
     def latest(self):
         return self.scores[-1]
 
@@ -9,11 +9,11 @@ class HighScores(object):
         return sorted(self.scores, reverse=True)[:3]
 
     def personal_best(self):
-        return sorted(self.scores, reverse=True)[0]
+        return max(self.scores)
 
     def report(self):
         last = self.latest()
         best = self.personal_best()
         diff = best - last
-        return "Your latest score was {}. ".format(last) + \
-            ("That's your personal best!", "That's {} short of your personal best!".format(diff))[diff > 0]
+        return f"Your latest score was {last}. " + \
+            ("That's your personal best!", f"That's {diff} short of your personal best!")[diff > 0]
