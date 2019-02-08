2eb499c443e63f7219254abc89fb04dd08f9200b
Pat Hermens <p@hermens.com.au>
2019-02-08 21:13:46 +0100

    Finished the series problem using list comprehension

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,10 @@
-def slices(series, length):
-    pass
+def slices(series="", length=1):
+
+    if not series:
+        raise ValueError("Series was an invalid value")
+    if length <= 0 or length > len(series):
+        raise ValueError("Length was an incorrect value")
+
+    return [series[i:i+length] \
+        for i in range(0, len(series), 1) \
+        if len(series[i:i+length]) == length]
