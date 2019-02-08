b096be1e1e9c73389f30e65051491436d3073795
Pat Hermens <p@hermens.com.au>
2019-02-08 21:28:35 +0100

    Updated to make it more pythonic

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,12 +1,7 @@
 class Matrix():
     def __init__(self, matrix_string):
-        # I dislike building the array myself, but numpy isn't available...?
-        # Also, this could be "more pythonic", but it gets very confusing.
-        self.matrix = []
-        for rownum, row in enumerate(matrix_string.split('\n')):
-            self.matrix.append([])
-            for item in row.split(' '):
-                self.matrix[rownum].append(int(item))
+        self.matrix = [[int(item) for item in row.split(' ')] \
+            for rownum, row in enumerate(matrix_string.split('\n'))]
 
     def row(self, index=1):
         return self.matrix[index - 1]
