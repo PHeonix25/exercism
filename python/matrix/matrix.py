f527fe0537c4880e4ad4620fc0e62cfa1aecf04c
Pat Hermens <p@hermens.com.au>
2019-02-08 21:30:46 +0100

    removes unnecessary variable

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,7 +1,7 @@
 class Matrix():
     def __init__(self, matrix_string):
         self.matrix = [[int(item) for item in row.split(' ')] \
-            for rownum, row in enumerate(matrix_string.split('\n'))]
+            for _, row in enumerate(matrix_string.split('\n'))]
 
     def row(self, index=1):
         return self.matrix[index - 1]
