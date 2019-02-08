c33c0aac0c2d8749cdf5457e01ebdc92d05a31c1
Pat Hermens <p@hermens.com.au>
2019-02-08 22:52:45 +0100

    Changed the naming to be more descriptive

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,6 +1,6 @@
 from functools import reduce
 
-def all_series_of_length(series, length):
+def chunked_series(series, length):
     return [series[i:i+length] \
         for i in range(0, len(series), 1) \
         if len(series[i:i+length]) == length]
@@ -13,6 +13,6 @@ def largest_product(series=None, size=0):
         raise ValueError("You can't have a span > 0 when series is empty")
 
     return max([ \
-        reduce(lambda x, y: x*y, \
-            [int(c) for c in s]) \
-                for s in all_series_of_length(series, size)])
+        reduce(lambda acc, inp: acc*inp, \
+            [int(c) for c in chunk]) \
+                for chunk in chunked_series(series, size)])
