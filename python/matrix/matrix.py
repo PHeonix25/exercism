a17cfc7fe8f657767ea171a86250e10bb04b4564
Pat Hermens <p@hermens.com.au>
2019-02-08 21:36:37 +0100

    Double underscores for privates

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,10 +1,10 @@
 class Matrix():
     def __init__(self, matrix_string):
-        self.matrix = [[int(item) for item in row.split(' ')] \
+        self.__matrix = [[int(item) for item in row.split(' ')] \
             for _, row in enumerate(matrix_string.split('\n'))]
 
     def row(self, index=1):
-        return self.matrix[index - 1]
+        return self.__matrix[index-1]
 
     def column(self, index=1):
-        return [rows[index-1] for rows in self.matrix]
+        return [rows[index-1] for rows in self.__matrix]
