429528312b58cdfcfd49223e54322ac052780ffe
Pat Hermens <p@hermens.com.au>
2019-02-13 22:15:57 +0100

    Removed debugging statements

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,30 +1,20 @@
 from collections import Counter
 
-# Score categories
-# Change the values as you see fit
-YACHT =  ('YACHT',  lambda dice: 50 if len(set(dice)) == 1 else 0)
-ONES =   ('ONES',   lambda dice: sum(x for x in dice if x == 1))
-TWOS =   ('TWOS',   lambda dice: sum(x for x in dice if x == 2))
-THREES = ('THREES', lambda dice: sum(x for x in dice if x == 3))
-FOURS =  ('FOURS',  lambda dice: sum(x for x in dice if x == 4))
-FIVES =  ('FIVES',  lambda dice: sum(x for x in dice if x == 5))
-SIXES =  ('SIXES',  lambda dice: sum(x for x in dice if x == 6))
-FULL_HOUSE = ('FULL HOUSE', \
-    (lambda dice: sum(dice) if sorted(Counter(dice).values()) == [2,3] else 0))
-FOUR_OF_A_KIND = ('FOUR OF A KIND', \
-    lambda dice: max(i*4 for i in \
-        [x[0] for x in Counter(dice).most_common(1) if x[1] >= 4] or [0]))
-LITTLE_STRAIGHT = ('LITTLE STRAIGHT', \
-    lambda dice: 30 if sorted(dice) == list(range(1, 6)) else 0)
-BIG_STRAIGHT = ('BIG STRAIGHT', \
-    lambda dice: 30 if sorted(dice) == list(range(2, 7)) else 0)
-CHOICE = ('CHOICE', lambda dice: sum(x for x in dice))
+YACHT =  lambda dice: 50 if len(set(dice)) == 1 else 0
+ONES =   lambda dice: sum(x for x in dice if x == 1)
+TWOS =   lambda dice: sum(x for x in dice if x == 2)
+THREES = lambda dice: sum(x for x in dice if x == 3)
+FOURS =  lambda dice: sum(x for x in dice if x == 4)
+FIVES =  lambda dice: sum(x for x in dice if x == 5)
+SIXES =  lambda dice: sum(x for x in dice if x == 6)
+CHOICE = lambda dice: sum(x for x in dice)
+
+FULL_HOUSE = lambda dice: sum(dice) \
+    if sorted(Counter(dice).values()) == [2,3] else 0
+FOUR_OF_A_KIND = lambda dice: max(i*4 for i in \
+    [x[0] for x in Counter(dice).most_common(1) if x[1] >= 4] or [0])
+LITTLE_STRAIGHT = lambda dice: 30 if sorted(dice) == list(range(1, 6)) else 0
+BIG_STRAIGHT = lambda dice: 30 if sorted(dice) == list(range(2, 7)) else 0
 
 def score(dice, category):
-    func = category[1]
-    print(f"Dice: {dice}, Category: {category}")
-    if func is None:
-        print(f"Requested category '{category[0]}' is not implemented yet.")
-    else:
-        print(f'Found category, providing {dice}; result is {func(dice)}')
-        return func(dice)
+    return category(dice)
