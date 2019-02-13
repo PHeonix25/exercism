8aac6be5d0a04a27e937d2e71b4a4f186b2caeee
Pat Hermens <p@hermens.com.au>
2019-02-13 22:21:47 +0100

    Finished optimising declarations

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,20 +1,24 @@
 from collections import Counter
 
 YACHT =  lambda dice: 50 if len(set(dice)) == 1 else 0
-ONES =   lambda dice: sum(x for x in dice if x == 1)
-TWOS =   lambda dice: sum(x for x in dice if x == 2)
-THREES = lambda dice: sum(x for x in dice if x == 3)
-FOURS =  lambda dice: sum(x for x in dice if x == 4)
-FIVES =  lambda dice: sum(x for x in dice if x == 5)
-SIXES =  lambda dice: sum(x for x in dice if x == 6)
+SUM_X =  lambda dice, x: sum(y for y in dice if y == x)
+ONES =   lambda dice: SUM_X(dice, 1)
+TWOS =   lambda dice: SUM_X(dice, 2)
+THREES = lambda dice: SUM_X(dice, 3)
+FOURS =  lambda dice: SUM_X(dice, 4)
+FIVES =  lambda dice: SUM_X(dice, 5)
+SIXES =  lambda dice: SUM_X(dice, 6)
 CHOICE = lambda dice: sum(x for x in dice)
 
 FULL_HOUSE = lambda dice: sum(dice) \
     if sorted(Counter(dice).values()) == [2,3] else 0
 FOUR_OF_A_KIND = lambda dice: max(i*4 for i in \
     [x[0] for x in Counter(dice).most_common(1) if x[1] >= 4] or [0])
-LITTLE_STRAIGHT = lambda dice: 30 if sorted(dice) == list(range(1, 6)) else 0
-BIG_STRAIGHT = lambda dice: 30 if sorted(dice) == list(range(2, 7)) else 0
+
+STRAIGHT = lambda dice, start, stop: \
+    bool(sorted(dice) == list(range(start, stop)))
+LITTLE_STRAIGHT = lambda dice: 30 if STRAIGHT(dice, 1, 6) else 0
+BIG_STRAIGHT = lambda dice: 30 if STRAIGHT(dice, 2, 7) else 0
 
 def score(dice, category):
     return category(dice)
