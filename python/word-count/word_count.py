556aa7b23aa175831bb9c2edb0c34c6dc886c03d
Pat Hermens <p@hermens.com.au>
2019-02-18 20:24:11 +0100

    Breaking into multiple lines

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,5 +1,7 @@
 import re, itertools
 
 def word_count(phrase=''):
-    phrase = sorted([word.strip('\'') for word in re.split("[^a-z0-9']+", phrase.lower().strip())])
-    return { key : sum(1 for x in group) for (key, group) in itertools.groupby(phrase) if key }
+    phrase = sorted([word.strip('\'') for word
+        in re.split("[^a-z0-9']+", phrase.lower().strip())])
+    return { key : sum(1 for x in group) for (key, group)
+        in itertools.groupby(phrase) if key }
