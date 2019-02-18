c8055d8d89fa3d18d33e98b494653a498677fc25
Pat Hermens <p@hermens.com.au>
2019-02-18 20:04:49 +0100

    Adds basic solution that passes tests

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,9 @@
+import re
+
 def word_count(phrase):
-    pass
+    wordcount = {}
+    clean_phrase = [word.strip('\'') for word in re.split("[^a-z0-9']+", phrase.lower().strip(), flags=re.IGNORECASE)]
+    for word in clean_phrase:
+        if word: 
+            wordcount[word] = wordcount.get(word, 0) + 1
+    return wordcount
