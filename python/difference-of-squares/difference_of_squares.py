d4768090e13bd442750dc6e75344ac9e66d3e688
Pat Hermens <p@hermens.com.au>
2019-02-18 20:40:36 +0100

    This one was simple

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,12 @@
-def square_of_sum(count):
-    pass
+def square_of_sum(count=1): 
+    """Returns the square of the sum of the numbers from 1 to count"""
+    return sum(range(1, count+1)) ** 2
+    
+def sum_of_squares(count=1):
+    """Returns the sum of the square of the numbers from 1 to count"""
+    return sum(i**2 for i in range(1, count+1))
 
-
-def sum_of_squares(count):
-    pass
-
-
-def difference(count):
-    pass
+def difference(count=1):
+    """Returns the difference between the square of the sum and \
+        the sum of the square of all numbers from 1 to count"""
+    return square_of_sum(count) - sum_of_squares(count)
