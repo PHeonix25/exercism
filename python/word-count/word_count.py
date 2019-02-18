3d2970206b1f27f53c0c79d8d489f15f0a327e3c
Pat Hermens <p@hermens.com.au>
2019-02-18 21:23:09 +0100

    Documenting the solution

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,10 +1,18 @@
 import re, itertools
 
 def word_count(phrase=''):
+    """
+    Counts "words" (as defined by [a-zA-Z0-9']) after stripping them,
+    by sorting them and passing them to 'groupby', and using the word
+    as the key value itself, and summing the number of values in the group.
+
+    Solution was not required to be case-sensitive, so everything
+    is converted to lower-case first in order to make parsing easier.
+    """
     lower = phrase.lower().strip()
     srted = sorted([word.strip('\'') 
         for word in re.split("[^a-z0-9']+", lower)])
 
     return { key : sum(1 for x in group) 
         for (key, group) in itertools.groupby(srted) 
-        if key }
+        if key } # TIP: this 'if key' removes blank keys
