29383801a69a5b927f5c7a585dbb637954ac4671
Pat Hermens <p@hermens.com.au>
2019-02-18 21:07:03 +0100

    Adding unoptimised solution

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,7 @@
-def find_anagrams(word, candidates):
-    pass
+def find_anagrams(word='', candidates=[]):
+    word = word.lower()
+    # I dislike how many times we call lower() below, 
+    # but some comparisons are case-sensitive when others aren't
+    return [c for c in candidates
+        if word != c.lower()
+        and sorted(word) == sorted(c.lower())]
