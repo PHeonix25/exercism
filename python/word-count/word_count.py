97747a81f2d00ed8d524493d2d2e765427de927e
Pat Hermens <p@hermens.com.au>
2019-02-18 21:17:56 +0100

    Updated formatting to make it read cleaner

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,7 +1,10 @@
 import re, itertools
 
 def word_count(phrase=''):
-    phrase = sorted([word.strip('\'') for word
-        in re.split("[^a-z0-9']+", phrase.lower().strip())])
-    return { key : sum(1 for x in group) for (key, group)
-        in itertools.groupby(phrase) if key }
+    lower = phrase.lower().strip()
+    srted = sorted([word.strip('\'') 
+        for word in re.split("[^a-z0-9']+", lower)])
+
+    return { key : sum(1 for x in group) 
+        for (key, group) in itertools.groupby(srted) 
+        if key }
