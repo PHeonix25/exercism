d1bcf95b0bedc83edf9fa5bf6c80cfe0cba2ade2
Pat Hermens <p@hermens.com.au>
2019-02-18 20:22:12 +0100

    Tidied it up, made it shorter

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,9 +1,5 @@
-import re
+import re, itertools
 
-def word_count(phrase):
-    wordcount = {}
-    clean_phrase = [word.strip('\'') for word in re.split("[^a-z0-9']+", phrase.lower().strip(), flags=re.IGNORECASE)]
-    for word in clean_phrase:
-        if word: 
-            wordcount[word] = wordcount.get(word, 0) + 1
-    return wordcount
+def word_count(phrase=''):
+    phrase = sorted([word.strip('\'') for word in re.split("[^a-z0-9']+", phrase.lower().strip())])
+    return { key : sum(1 for x in group) for (key, group) in itertools.groupby(phrase) if key }
