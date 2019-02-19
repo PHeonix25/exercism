7399fbd7dc18d51451c1b8a473c1967e2952846e
Pat Hermens <p@hermens.com.au>
2019-02-19 21:16:13 +0100

    Added note about the length assumption

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,8 +1,9 @@
 def distance(strand_a=[], strand_b=[]):
     """
     Compares two strands, and returns the count of their differences.
-
-    Raises ValueError if strands are not the same length.
+    
+    ASSUMES strands are short enough to be evaluated for their length.
+    RAISES ValueError if strands are not the same length.
     """
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands are not the same length. Cannot compare.")
