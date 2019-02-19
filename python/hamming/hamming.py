e65e212767ac3a383284fea8641b221d15a41020
Pat Hermens <p@hermens.com.au>
2019-02-19 21:11:33 +0100

    Adding solution, simple one-liner!

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,10 @@
-def distance(strand_a, strand_b):
-    pass
+def distance(strand_a=[], strand_b=[]):
+    """
+    Compares two strands, and returns the count of their differences.
+
+    Raises ValueError if strands are not the same length.
+    """
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands are not the same length. Cannot compare.")
+    
+    return sum(1 for (x,y) in zip(strand_a, strand_b) if x != y)
