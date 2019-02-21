4f3db39c43bc4cf3237ecfe70174c5bb5f3d495e
Pat Hermens <p@hermens.com.au>
2019-02-21 21:46:07 +0100

    Adding solution for finding prime factors with recursion

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,18 @@
 def prime_factors(natural_number):
-    pass
+    """
+    Uses recursion to calculate all 
+    prime factors for a given number.
+    """
+    if not natural_number > 0:
+        raise ValueError("Provided value needs to be greater than 0")
+
+    if natural_number == 1:
+        return []
+
+    # Start at 2 to prevent divmod from overflowing
+    for i in range(2, natural_number):
+        x,y = divmod(natural_number, i) 
+        if not y: 
+            return [i] + prime_factors(x)
+
+    return [natural_number]
