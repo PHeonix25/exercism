8ecdfa8f7f57815c422b975008290b45bf10e8e1
Pat Hermens <p@hermens.com.au>
2019-02-21 21:46:18 +0100

    Adding tests to evaluate stupid input

--- python/prime-factors/prime_factors_test.py
+++ python/prime-factors/prime_factors_test.py
@@ -6,6 +6,14 @@ from prime_factors import prime_factors
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
 
 class PrimeFactorsTest(unittest.TestCase):
+    def test_invalid_input(self):
+        with self.assertRaisesWithMessage(ValueError):
+            prime_factors(-1)
+    
+    def test_zero(self):
+        with self.assertRaisesWithMessage(ValueError):
+            prime_factors(0)
+
     def test_no_factors(self):
         self.assertEqual(prime_factors(1), [])
 
@@ -27,6 +35,15 @@ class PrimeFactorsTest(unittest.TestCase):
     def test_factors_include_a_large_prime(self):
         self.assertEqual(prime_factors(93819012551), [11, 9539, 894119])
 
+    # Utility functions
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
 
 if __name__ == '__main__':
     unittest.main()
