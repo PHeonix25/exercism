3155dd0df941fa22893a9114fbe02e9c337bba6f
Pat Hermens <p@hermens.com.au>
2019-02-21 20:38:12 +0100

    Given a number, converts it to raindrop-speak

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,36 @@
+from math import sqrt
+
+def __factors(number):
+    """
+    Calculates all factors of a number.
+
+    Loosely based off https://stackoverflow.com/a/6800214, 
+        but removing the private `list.__add__` method 
+        invocation and using `not` instead of `number==0`.
+    """
+    return set(x for tup in 
+        ([i, number//i] for i in 
+            range(1, int(sqrt(number))+1) 
+            if not number % i) 
+        for x in tup)
+
 def raindrops(number):
-    pass
+    """
+    Converts a given number into "raindrop-speak". 
+    (similar to FizzBuzz: https://en.wikipedia.org/wiki/Fizz_buzz)
+    
+    We calculate factors of the given number, hold them in a 
+    list, then use `in` comparisons to add to our returned value.
+
+    ASSUMES that the list of factors is normally small. 
+    """
+    result = ''
+    factors = list(__factors(number))
+    print(f"Given {number}, factors are {factors}.")
+
+    if 3 in factors: result += "Pling"
+    if 5 in factors: result += "Plang"
+    if 7 in factors: result += "Plong"
+    if not result: result = str(number)
+
+    return result
